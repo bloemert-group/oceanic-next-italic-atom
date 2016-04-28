2ada85e21c4afb611c898bc05dc36afdc823d084
Jarno Nijboer <jarno@nijboer.me>
2016-04-28 22:55:46 +0200

    Add update script for theme

---
+++ scripts/update.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+cd %~dp0
+cd ../
+
+echo ## Updating theme to latest version
+call git submodule update --remote --merge
+
+echo ## Generate theme again using APM
+apm init --theme . --convert ./theme/Oceanic Next Italic.tmTheme
+
+echo ## Commiting updated theme
+call git commit -am "Updated theme"
+call git push
+echo Done
